fb197cfe282506596777c4b76d60b52bb14209e2
KwongWaiKin <waikin901@gmail.com>
2025-05-28 17:59:30 +0800

    Fixed layouttsx file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import { Stack } from 'expo-router';
-=======
 import { Stack } from "expo-router";
 import './globals.css'
->>>>>>> 55734c13a51f55fbce1b22e412475f3ae698e908
 
 export default function RootLayout() {
   return (
